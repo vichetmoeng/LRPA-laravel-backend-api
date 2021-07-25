d46a570b6812ae5a298f6c2da8d002bb43c7b2ff
Vichet MOENG <v.moeng@testingstudio.io>
2021-07-26 06:41:21 +0700

    add login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+        try {
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $user = Auth::user();
+                $token = $user->createToken('app')->accessToken;
+
+                return response([
+                    'message' => "Successfully Login",
+                    'token' => $token,
+                    'user' => $user
+                ], 200);
+            }
+        } catch (\Exception $exception) {
+            return response([
+                'message' => $exception->getMessage()
+            ], 400);
+        }
+
+        return response([
+            'message' => 'Invalid Email or Password'
+        ], 401);
+    } // end method
 }
